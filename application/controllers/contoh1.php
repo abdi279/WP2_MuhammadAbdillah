b3207d532762191665ab37d816efe98c3e7b4ced
unknown <mabdillah279@gmail.com>
2022-03-21 18:02:50 +0700

    menambahkan file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Abdillah,
+            Saya tinggal di daerah Talas 5,
+            Olahraga yang saya sukai adalah 
+            Bulutangkis";
+    }
+}
